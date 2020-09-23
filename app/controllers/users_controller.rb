fb4090b2ed04e5154c18b7703216ca5c227ce113
DamonLC21 <chiversdamon@gmail.com>
2020-09-23 11:01:45 -0600

    auth pt.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
     def index 
         @users = User.all 
         render json: @users
@@ -12,5 +11,16 @@ class UsersController < ApplicationController
         )
 
         render json: @user
+    end
+
+    def login  
+        @user = User.find_by(username: params[:username])
+
+        if @user && @user.authenticate(params[:password])
+            @token = JWT.encode({user_id: @user.id}, Rails.application.secrets.secret_key_base[0])
+            render json: {user: @user, token: @token}
+        else
+            render json: {message: "Invalid credentials!"}
+        end
     end
 end
